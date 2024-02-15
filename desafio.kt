b43c1b2acfdf9a820a4622f225e7b886d6a22c18
“Romário <nz.romariodasilva@gmail.com>
2024-02-15 19:23:11 +0100

    Enviando o Desafio no Git

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,86 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import kotlin.collections.emptyList
+import kotlin.collections.listOf
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    PRINCIPIANTE,
+    INTERMEDIARIO,
+    AVANCADO,
+    TODOS_NIVEIS
+}
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val codigo: Int, val nome: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(
+        val codigo: Int,
+        val nome: String,
+        val nivel: Nivel,
+        val duracao: Int,
+        var conteudos: List<ConteudoEducacional> = emptyList()
+) {
 
     val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach { inscritos.add(it) }
+    }
+
+    fun gerarFicha() {
+        print(
+                """
+        ============== $codigo - $nome ==============
+        Inscritos: ${inscritos.size}
+        Duração: $duracao horas com ${conteudos.size} modulos
+        Nivel: $nivel
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        Programa:
+        """
+        )
+        println()
+        conteudos.forEach { (_codigo, _nome) -> println("\t\t $_codigo - $_nome.") }
+        println()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    // ------------------------- Formação
+    val curso1 =
+            Formacao(1, "Mergulho em Sql", Nivel.TODOS_NIVEIS, 60).apply {
+                conteudos =
+                        listOf(
+                                ConteudoEducacional(1, "Noções básicas e histórial"),
+                                ConteudoEducacional(2, "DDL - Data Definition Language"),
+                                ConteudoEducacional(3, "DML - Data Manipulation Language"),
+                                ConteudoEducacional(4, "DQL - Data Query Language"),
+                                ConteudoEducacional(5, "DCL - Data Definition Language"),
+                                ConteudoEducacional(6, "Projecto Final")
+                        )
+            }
+
+    val curso2 =
+            Formacao(2, "Desenvolvendo uma Rest API com Spring", Nivel.INTERMEDIARIO, 20).apply {
+                conteudos =
+                        listOf(
+                                ConteudoEducacional(1, "Fundamentos sobre Rest APIs"),
+                                ConteudoEducacional(2, "Introdução ao Spring"),
+                                ConteudoEducacional(3, "Criando o primeiro projecto"),
+                                ConteudoEducacional(4, "Introdução ao Spring Data"),
+                                ConteudoEducacional(5, "Realizando o CRUD"),
+                                ConteudoEducacional(6, "Implementando boas práticas"),
+                                ConteudoEducacional(7, "Testes unitarios"),
+                                ConteudoEducacional(8, "Deploy na AWS e Heroku"),
+                        )
+            }
+
+    // -------------------------- Matriculas
+    curso1.matricular(Usuario(), Usuario(), Usuario(), Usuario())
+    curso2.matricular(Usuario(), Usuario(), Usuario(), Usuario(), Usuario(), Usuario(), Usuario())
+
+    // -------------------------- Visualizando o resultado
+
+    println("\n\t********************** Fichas Formativas **********************")
+    curso1.gerarFicha()
+    curso2.gerarFicha()
 }
